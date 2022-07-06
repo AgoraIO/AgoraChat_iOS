97ef2c1b7966db5fcf73d778acda5f66702ea295
朱继超 <984065974@qq.com>
2022-07-06 16:22:32 +0800

    change check sum and readme
    change check sum and readme

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,8 +10,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "AgoraChat_iOS",
-            targets: ["AgoraChat_iOS"]),
+            name: "AgoraChat",
+            targets: ["AgoraChat"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,6 +20,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "AgoraChat_iOS", url: "https://download.agora.io/swiftpm/Agora_Chat_iOS/1.0.6/AgoraChat.xcframework.zip", checksum: "ec0d9c92453b4927d344cff35a412c70028fda324d701d933f0ae27f7e935294"),
+        .binaryTarget(name: "AgoraChat", url: "https://download.agora.io/swiftpm/Agora_Chat_iOS/1.0.6/AgoraChat.xcframework.zip", checksum: "ab5ea482a23d696e8e2e542b82d5937588a6e673654ed92549f333d285f48815"),
     ]
 )
